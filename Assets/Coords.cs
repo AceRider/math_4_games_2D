7874e5c1526ae26f94c4a09df9dc3a898a6305f8
aecio <aecio.jorge@gmail.com>
2019-12-09 16:55:49 -0400

    update

---
+++ Assets/Coords.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coords
+{
+    float x;
+    float y;
+    float z;
+
+    public Coords(float _X, float _Y)
+    {
+        x = _X;
+        y = _Y;
+        z = -1;
+    }
+
+    public override string ToString()
+    {
+        return "(" + x + ","+ y + "," + z + ")";
+    }
+}
---
+++ Assets/DrawLines.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLines : MonoBehaviour
+{
+    Coords point = new Coords(10, 20);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(point.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
