41aa79f3a667ab34c20be92a9d79e3a7ea11ee32
Aecio Jorge <aecio.jorge@gmail.com>
2019-12-09 22:43:47 -0400

    3.19

--- Assets/Coords.cs
+++ Assets/Coords.cs
@@ -19,4 +19,17 @@ public class Coords
     {
         return "(" + x + ","+ y + "," + z + ")";
     }
+
+    public static void DrawPoint(Coords position, float width, Color colour)
+    {
+        GameObject line = new GameObject("Point_" + position.ToString());
+        LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+        lineRenderer.material.color = colour;
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, new Vector3(position.x - width / 3.0f, position.y - width / 3.0f, position.z));
+        lineRenderer.SetPosition(1, new Vector3(position.x + width / 3.0f, position.y + width / 3.0f, position.z));
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+    }
 }
