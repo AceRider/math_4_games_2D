4203352cb57ebeba86662e68cf3d2cd0503689d8
Aecio Jorge <aecio.jorge@gmail.com>
2019-12-13 19:17:34 -0400

    3.25

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -6,38 +6,55 @@ using UnityEngine.UI;
 
 public class Drive : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
-    public Text energyAmt;
-    Vector3 currentLocation;
+    #region Course 3
+    // public float speed = 10.0f;
+    // public float rotationSpeed = 100.0f;
+    // public Text energyAmt;
+    // Vector3 currentLocation;
 
-    private void Start()
+    //private void Start()
+    //{
+    //    currentLocation = this.transform.position;
+    //}
+
+    //void Update()
+    //{
+    //    if (float.Parse(energyAmt.text) <= 0) return;
+
+    //    // Get the horizontal and vertical axis.
+    //    // By default they are mapped to the arrow keys.
+    //    // The value is in the range -1 to 1
+    //    float translation = Input.GetAxis("Vertical") * speed;
+    //    float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+    //    // Make it move 10 meters per second instead of 10 meters per frame...
+    //    translation *= Time.deltaTime;
+    //    rotation *= Time.deltaTime;
+
+    //    // Move translation along the object's z-axis
+    //    transform.Translate(0, translation, 0);
+
+    //    // Rotate around our y-axis
+    //    transform.Rotate(0, 0, -rotation);
+
+    //    energyAmt.text = (float.Parse(energyAmt.text) - Vector3.Distance(currentLocation, this.transform.position)) + "";
+
+    //    currentLocation = this.transform.position;
+    //}
+    #endregion
+    #region Course 4
+
+    Vector3 dir = new Vector2(-0.1f, 0.1f);
+    private void Update()
     {
-        currentLocation = this.transform.position;
+        Vector3 position = this.transform.position;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            position.x += dir.x;
+            position.y += dir.y;
+        }
+        this.transform.position = position;
     }
+    #endregion
 
-    void Update()
-    {
-        if (float.Parse(energyAmt.text) <= 0) return;
-
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        // Move translation along the object's z-axis
-        transform.Translate(0, translation, 0);
-
-        // Rotate around our y-axis
-        transform.Rotate(0, 0, -rotation);
-
-        energyAmt.text = (float.Parse(energyAmt.text) - Vector3.Distance(currentLocation, this.transform.position)) + "";
-
-        currentLocation = this.transform.position;
-    }
 }
