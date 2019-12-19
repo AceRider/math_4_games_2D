d9036236f84c94f613146d81ac02b925296cc8ae
Aecio Jorge <aecio.jorge@gmail.com>
2019-12-18 21:32:47 -0400

    4.35

--- Assets/ObjectManager.cs
+++ Assets/ObjectManager.cs
@@ -9,12 +9,12 @@ public class ObjectManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        GameObject obj = Instantiate(objPrefab, new Vector3(Random.Range(-100, 100), 
-                                                            Random.Range(-100, 100), 
-                                                            objPrefab.transform.position.z), 
-                                                            Quaternion.identity);
-        //Debug.Log("Fuel Location: " + obj.transform.position);
-        objPosition = obj.transform.position;
+        //GameObject obj = Instantiate(objPrefab, new Vector3(Random.Range(-100, 100), 
+        //                                                    Random.Range(-100, 100), 
+        //                                                    objPrefab.transform.position.z), 
+        //                                                    Quaternion.identity);
+        ////Debug.Log("Fuel Location: " + obj.transform.position);
+        //objPosition = obj.transform.position;
     }
 
     // Update is called once per frame
